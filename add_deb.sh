a14cc54845fcdf5520d93c1d25018fdc2a989b40
laoyening <1337849843@qq.com>
2021-01-08 10:53:14 +0800

    add carp-imx6 mirror
    Signed-off-by: laoyening <1337849843@qq.com>

--- add_deb.sh
+++ add_deb.sh
@@ -1,2 +1,16 @@
 #!/bin/bash
-reprepro -Vb . includedeb buster $1
+
+usage () {
+	echo "usage: ./add_deb.sh buster/carp-imx6 xxx.deb"
+}
+
+while [ ! -z "$1" ] ; do
+	case $1 in
+	-h|--help)
+		usage
+		exit
+		;;
+	esac
+done
+
+reprepro --ask-passphrase -Vb . includedeb $1 $2
--- sub_deb.sh
+++ sub_deb.sh
@@ -1,2 +1,14 @@
 #!/bin/bash
-reprepro -Vb . remove buster $1
+usage () {
+	echo "usage: ./sub_deb.sh buster/carp-imx6 xxx"
+}
+
+while [ ! -z "$1" ] ; do
+	case $1 in
+	-h|--help)
+		usage
+		exit
+		;;
+	esac
+done
+reprepro -Vb . remove $1 $2
